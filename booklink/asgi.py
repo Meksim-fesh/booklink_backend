66844852a73fcbef2e20f9034c0d20978f86f2c0
Maksym Nimych <maksimnimitch@gmail.com>
2024-12-28 17:01:30 +0200

    Change the quotes for manage.py and booklink folder with black module

--- booklink/asgi.py
+++ booklink/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booklink.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booklink.settings")
 
 application = get_asgi_application()
