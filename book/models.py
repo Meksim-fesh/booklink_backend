dd3b166c831563c5b95be66323baa9fd7968b1a5
Maksym Nimych <maksimnimitch@gmail.com>
2025-01-03 17:10:40 +0200

    Add book related models (Genre, Author, Chapter, Commentary, Book)

--- book/models.py
+++ book/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+
+class Book(models.Model):
+    # cover = models.ImageField()
+    name = models.CharField(max_length=255)
+    genres = models.ManyToManyField(Genre, related_name="books")
+    authors = models.ManyToManyField(Author, related_name="books")
+    pages = models.IntegerField()
+    summary = models.TextField()
+    likes = models.IntegerField()
+    views = models.IntegerField()
+    views_per_month = models.IntegerField()
+
+
+class Chapter(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    link = models.URLField()
+
+
+class Commentary(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    content = models.CharField(max_length=1200)
+    # user = models.ForeignKey()
+    date = models.DateField(auto_now_add=True)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
